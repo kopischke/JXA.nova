ba85ab6fb6dc6cc4e9d73e0299a561bff6d44815
Martin Kopischke <martin@kopischke.net>
2020-10-16 15:45:37 +0200

    Ensure source integrity in commands and tasks
    JavaScript has very odd ideas about escape characters (like `\n`, which led to code containing them in strings, comments or regular expression literals not being processed correctly.

--- JXA.novaextension/Scripts/lib/commands.js
+++ JXA.novaextension/Scripts/lib/commands.js
@@ -8,25 +8,28 @@ const { binDir } = require('./extension')
  * @param {string} source - The JXA source to send to Script Editor.
  */
 function jxaToEditor (source) {
-  const escape = /(`|\$\{|\\[nr])/g
+  // We have to do a little dance to get all code properly into Script Editor:
+  // - using a raw template string ensures Script Editor leaves literal '\n' and such alone;
+  // - the `escaped` step makes sure the source does not break out of the template string;
+  // - the final `replace` removes these escapes once the code has landed in Script Editor.
+  const escaped = source.replace(/(`|\$\{)/g, '\\$1')
   const script = [
     'const edi = Application("Script Editor")',
     'const doc = edi.make({ new: "document" })',
-    'doc.contents = `' + source.replace(escape, '\\\\$1') + '`',
+    'doc.contents = String.raw`' + escaped + '`.replace(/\\\\(`|\\$\\{)/g, "$1")',
     'edi.activate()',
     'doc.checkSyntax()'
   ]
 
-  const args = {
-    args: ['-'],
-    env: { JXARUN_CODE: `${script.join('\n')}` }
-  }
-
-  const runner = new Process(nova.path.join(binDir, 'jxarun'), args)
+  const runner = new Process(nova.path.join(binDir, 'jxarun'), { args: ['-'] })
   const stderr = []
   runner.onStderr(line => stderr.push(line))
   runner.onDidExit(code => { if (code > 0) console.error(stderr.join('')) })
   runner.start()
+
+  const writer = runner.stdin.getWriter()
+  writer.write(script.join('\n'))
+  writer.close()
 }
 
 module.exports = { jxaToEditor: jxaToEditor }
